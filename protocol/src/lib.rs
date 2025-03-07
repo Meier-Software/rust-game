2b9ccb97f526760133263c7db9d2aa1ac2f70429
Able <abl3theabove@gmail.com>
2025-03-07 00:41:26 -0600

    PROTO

--- protocol/src/lib.rs
+++ protocol/src/lib.rs
@@ -1,10 +1,20 @@
-use specs::{World, WorldExt};
+use specs::{Component, VecStorage, World, WorldExt};
 
+#[derive(Component)]
+#[storage(VecStorage)]
 pub struct Position {
     pub x: f32,
     pub y: f32,
 }
+impl Position {
+    pub fn new(x: f32, y: f32) -> Self {
+        Self { x, y }
+    }
+}
 
+pub fn world_register(world: &mut World) {
+    world.register::<Position>();
+}
 
 pub enum Facing {
     North,
@@ -19,15 +29,11 @@ pub enum ServerToClient {
 
 pub enum ClientToServer {
     AttemptPlayerMove(Position),
-    AttemptPlayerFacingChange(Facing)
+    AttemptPlayerFacingChange(Facing),
 }
 
+pub fn init_world() -> World {
+    let mut world = World::new();
 
-
-pub fn init_world()->World{
-     let mut world = World::new();
-     
-
-
-     world
+    world
 }
